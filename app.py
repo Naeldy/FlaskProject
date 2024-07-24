fe2b18c97c1832faf608469d49d4d57781645db3
Matmjr <matmj@cin.ufpe.br>
2024-07-23 21:14:51 -0300

    primeira aula

--- app.py
+++ app.py
@@ -1,1 +1,14 @@
-print("Teste")
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return "Olá"
+
+@app.route("/teste")
+def teste():
+    return "Página de Teste"
+
+if __name__ == "__main__":
+    app.run()
